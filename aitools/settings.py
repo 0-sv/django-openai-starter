ce3e84d8b85c810e23ac59185181c263a1ef35e9
A.J.S. Verbeek <ajsverbeek@gmail.com>
2023-01-03 15:54:26 +0100

    feat: authuser

--- aitools/settings.py
+++ aitools/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sql.apps.SqlConfig',
+    'authuser'
 ]
+
+AUTH_USER_MODEL = "authuser.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
