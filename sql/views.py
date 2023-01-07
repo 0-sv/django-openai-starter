e7b25e6cf8f384247e7d85720eb0124649dd0a5b
A.J.S. Verbeek <ajsverbeek@gmail.com>
2023-01-07 14:45:45 +0100

    feat: super basic output

--- sql/views.py
+++ sql/views.py
@@ -4,5 +4,11 @@ from .forms import QuestionForm
 
 
 def index(request):
-    form = QuestionForm()
-    return render(request, 'sql/poc.html', {'form': form})
+    output = "waiting for input"
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            output = "some generated text"
+    else:
+        form = QuestionForm()
+    return render(request, 'sql/poc.html', {'form': form, 'output': output})
